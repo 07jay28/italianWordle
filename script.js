f538420ce64d8a9c81712373b857068d62ee03cd
07jay28 <collegektejas@gmail.com>
2025-03-10 14:35:18 -0500

    add node module and gui

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ let currentGuess = [];
 let nextLetter = 0;
 let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
 console.log(rightGuessString)
+
+
+function initBoard() {
+    let board = document.getElementById("game-board")
+
+    for(let i = 0; i < TOTAL_GUESSES; i++) {
+        let row = document.createElement("div")
+        row.className = "rowLetter"
+
+        for(let j = 0; j < 5; j++) {
+            let box = document.createElement("div")
+            box.className = "boxLetter"
+            row.append(box)
+        }
+
+        board.append(row)
+    }
+}
+
+initBoard()
